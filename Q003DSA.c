72e66717e8e43407bf7498dfdf4d505e06255cff
Agrima Khandelwal <agrima19feb@gmail.com>
2026-02-03 23:48:46 +0530

    Add files via upload

---
+++ Q003DSA.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, k, i, comparisons = 0;
+    int found = 0;
+
+    if (scanf("%d", &n) != 1) return 0;
+    int arr[n];
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+    for (i = 0; i < n; i++) {
+        comparisons++;
+        if (arr[i] == k) {
+            printf("Found at index %d\n", i);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("Not Found\n");
+    }
+
+    printf("Comparisons = %d\n", comparisons);
+    return 0;
+}
